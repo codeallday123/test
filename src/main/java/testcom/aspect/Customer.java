02e5744e3127ee717030f8f7f2e88be6ba3807be
P3026236 <P3026236@CORTOPSAL1114T5.CORP.CHARTERCOM.com>
2021-06-17 09:05:25 -0500

    changed pom

--- src/main/java/testcom/aspect/Customer.java
+++ src/main/java/testcom/aspect/Customer.java
@@ -2,22 +2,22 @@ package testcom.aspect;
 
 public class Customer {
 
-	 private final String name;
-	    private final int age;
+	private final String name;
+	private final int age;
 
-	    public Customer(final String name, final int age) {
-	        this.name = name;
-	        this.age = age;
-	    }
+	public Customer(final String name, final int age) {
+		this.name = name;
+		this.age = age;
+	}
 
-	    /** javadoc */
-	    public String getName() {
-	        System.out.println("Inside method getName(), Name is: " + name);
-	        return name;
-	    }
+	/** javadoc */
+	public String getName() {
+		System.out.println("Inside method getName(), Name is: " + name);
+		return name;
+	}
 
-	    /** javadoc */
-	    public int getAge() {
-	        return age;
-	    }
+	/** javadoc */
+	public int getAge() {
+		return age;
+	}
 }
